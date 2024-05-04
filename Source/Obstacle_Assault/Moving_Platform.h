3e31551e8021f6f0d1785585d45b7096bbdfa0e4
mor101090 <mor101090@gmail.com>
2024-05-04 18:33:53 +0300

    [video 46]: Using Structs In C

--- Source/Obstacle_Assault/Moving_Platform.h
+++ Source/Obstacle_Assault/Moving_Platform.h
@@ -23,35 +23,11 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EDITAnywhere)
-	int32 MyInt = 99;
+	UPROPERTY(EditAnywhere)
+	FVector MyVector = FVector(1, 2, 3);
 
-	
-	UPROPERTY(EDITAnywhere)
-	int32 InputA = 0;
+	UPROPERTY(EditAnywhere)
+	float MyX = 0;
 
-	UPROPERTY(EDITAnywhere)
-	int32 InputB = 0;
-
-	UPROPERTY(EDITAnywhere)
-	int32 APlusB = 0;
-
-	UPROPERTY(EDITAnywhere)
-	float InputFloatA = 0;
-
-	UPROPERTY(EDITAnywhere)
-	float InputFloatB = 0;
-
-	
-	UPROPERTY(EDITAnywhere)
-	float APlusBFloat = 0;
-
-	
-
-		UPROPERTY (EDITANYWHERE)
-			float MyFloat = 5.99;
-
-			UPROPERTY (EditANYWHERE)
-			bool MyBool = true;
 
 };
